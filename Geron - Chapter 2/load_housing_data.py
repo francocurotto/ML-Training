192620586644a6ba5bc30f65f3b0d66e37c289f7
Franco Curotto <francocurotto@gmail.com>
2022-10-06 17:51:34 -0300

    first commit

---
+++ Geron - Chapter 2/load_housing_data.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+from fetch_housing_data import HOUSING_PATH
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
