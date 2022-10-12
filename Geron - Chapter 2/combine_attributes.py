103e70d8fab76d8390a7f48a0bc60af8799d12f2
Franco Curotto <francocurotto@gmail.com>
2022-10-12 14:16:29 -0300

    add combine

---
+++ Geron - Chapter 2/combine_attributes.py
@@ -0,0 +1,5 @@
+def combine_attributes(housing):
+    housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+    housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+    housing["population_per_household"] = housing["population"]/housing["households"]
+    return housing
